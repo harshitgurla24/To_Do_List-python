70488d27fb789c013e06b67150d63a08e7afebf1
harshitgurla24 <harshitgurla24@navgurukul.org>
2025-12-30 18:46:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        task = request.form.get("task")
+        if task:
+            tasks.append(task)
+        return redirect("/")
+    
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:index>", methods=["GET"])
+def delete(index):
+    if 0 <= index < len(tasks):
+        
+        tasks.pop(index)
+    return redirect("/")
+
+if __name__ == "__main__":  
+    app.run(debug=True)
